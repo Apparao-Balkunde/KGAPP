9cf0f88daaa55f9d6b66ede29add7aeb9ad54328
APPARAO BALKUNDE <apparaobalkunde901@gmail.com>
2025-06-21 20:19:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -53,4 +53,5 @@ def handle_disconnect():
         emit('update-positions', {'id': request.sid, 'x': -1, 'z': -1}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', 
+port=int(os.environ.get("PORT", 5000)))
